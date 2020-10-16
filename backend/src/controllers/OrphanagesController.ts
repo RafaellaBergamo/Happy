4fe5b62e31c7f3d998e1da6f5fded5f8e93c8355
Rafaella Bergamo <rafaellabergamo3@gmail.com>
2020-10-16 02:04:29 -0300

    Add validação nos campos

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -2,6 +2,7 @@ import {getRepository} from 'typeorm';
 import Orphanage from "../models/Orphanage";
 import {Request, Response} from 'express';
 import OrphanageView from "../views/orphanages_view";
+import * as Yup from "yup";
 
 export default {
     async index (request: Request, response: Response)
@@ -40,8 +41,8 @@ export default {
          const images = requestImages.map(image => {
              return {path: image.filename}
          })
-      
-         const orphanage = orphanagesRepository.create({
+
+         const data = {
             name, 
             latitude, 
             longitude, 
@@ -50,7 +51,24 @@ export default {
             opening_hours,
             open_on_weekends,
             images
+         }
+         const schema = Yup.object().shape({
+             nome: Yup.string().required(),
+             latitude: Yup.number().required(),
+             longitude: Yup.number().required(),
+             about: Yup.string().required().max(300),
+             instruction: Yup.string().required(),
+             opening_hours: Yup.string().required(),
+             open_on_weekends: Yup.string().required(),
+             images: Yup.array(Yup.object().shape({
+                 path: Yup.string().required()
+             }))
+         })
+         
+         await schema.validate(data, {
+             abortEarly: false,
          });
+         const orphanage = orphanagesRepository.create(data);
       
          await orphanagesRepository.save(orphanage);
          return response.status(201).json(orphanage);
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,18 @@
 import express from 'express';
 import path from 'path';
 import 'express-async-errors';
-
 import './database/connection';
+import errorHandler from "./errors/handler";
+import cors from "cors";
 
 import routes from './routes';
 
 
 const app = express ();
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname,'..', 'uploads')))
+app.use(errorHandler);
 
 app.listen(3333); //porta para visualizar o back localhost:3333
